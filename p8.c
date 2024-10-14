d47fd89ab07a386e791546ba116006fbb663ed14
pauso639 <sofiapaulinalopezgonzalez@gmail.com>
2024-10-13 22:31:41 -0600

    Update p8.c

--- p8.c
+++ p8.c
@@ -3,10 +3,66 @@ Laboratorio practica 8
 tablas de multiplicar*/
 
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 
-void mult_for(int n);
+void mult_for(int n)
 {
   int resp;
+  
+  for(int i = 1; i <= 10; i++)
+  {
+    resp = n * i;
+    printf("%d * %d = %d\n", n, i, resp);
+  }
+  //return 0;
+}
 
-for
+void mult_while(int n)
+{
+    int resp;
+    int i = 1;
+
+    while (i <= 10)
+    {
+        resp = n * i;
+        printf("%d * %d = %d\n", n, i, resp);
+        i = i + 1;
+    }
+    
+}
+
+void mult_dowhile(int n)
+{
+    int resp;
+    int i = 1;
+
+    do
+    {
+        resp = n * i;
+        printf("%d * %d = %d\n", n, i, resp);
+        i = i + 1;
+    } while (i <= 10);
+    
+}
+
+int main()
+{
+    int n;
+  
+    printf("Dame el numero de la tabla a multiplicar: ");
+    scanf("%d", &n);
+
+    printf("\n\n\n");
+    printf("Utilizacion del for\n");
+    mult_for(n);
+
+    printf("\n\n\n");
+    printf("Utilizacion del while\n");
+    mult_while(n);
+    
+    printf("\n\n\n");
+    printf("Utilizacion del do-while\n");
+    mult_dowhile(n);
+
+    return 0;
+}
